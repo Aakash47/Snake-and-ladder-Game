fa1455ac39b7bb227e8ca7b1c30a01f583a54658
aakash <aakashy613@gmail.com>
2023-11-27 17:13:52 +0530

    added more rules to game

--- SNL.cpp
+++ SNL.cpp
@@ -26,38 +26,107 @@ int main(){
     {
         system("cls");
         cout<<"Snake and Ladder Game!!!"<<endl;
-        cout<<"Current palyer "<<player+1<<endl;
+        cout<<"Palyer "<<player+1<<" chance."<<endl;
+        cout<<"Press Enter to roll a dice";
+        cin.ignore();
         cout<<"=================================="<<endl;
+        cout<<"Player "<<player+1<<" rolled a "<<dice()<<endl;
+
+        cout<<"=================================="<<endl;
+
+        if (current[player]==0 && dice()!=6)
+        {
+            // current[player] -= dice();
+            cout<<"Game not started for player "<<player+1<<" as player not get 6 to start the game"<<endl;
+        }
+        else
+        {
+            current[player] += dice();
+        }
+        
+        switch (current[player])
+        {
+        // cases to climbe ladders
+        case 5:
+            current[player]=10;
+            cout<<"player "<<player+1<<" climbed a ladder to 10"<<endl;
+            break;
+        case 20:
+            current[player]=45;
+            cout<<"player "<<player+1<<" climbed a ladder to 45"<<endl;
+            break;
+        case 35:
+            current[player]=42;
+            cout<<"player "<<player+1<<" climbed a ladder to 42"<<endl;
+            break;
+        case 50:
+            current[player]=63;
+            cout<<"player "<<player+1<<" climbed a ladder to 63"<<endl;
+            break;
+        case 65:
+            current[player]=76;
+            cout<<"player "<<player+1<<" climbed a ladder to 76"<<endl;
+            break;
+        case 70:
+            current[player]=87;
+            cout<<"player "<<player+1<<" climbed a ladder to 87"<<endl;
+            break;
+        case 83:
+            current[player]=96;
+            cout<<"player "<<player+1<<" climbed a ladder to 96"<<endl;
+            break;
+
+        // cases for snake bite
+        case 15:
+            current[player]=2;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 2"<<endl;
+            break;
+        case 25:
+            current[player]=10;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 10"<<endl;
+            break;
+        case 37:
+            current[player]=29;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 29"<<endl;
+            break;
+        case 52:
+            current[player]=45;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 45"<<endl;
+            break;
+        case 68:
+            current[player]=55;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 55"<<endl;
+            break;
+        case 85:
+            current[player]=71;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 71"<<endl;
+            break;
+        case 98:
+            current[player]=37;
+            cout<<"player "<<player+1<<" is bitten by snake and is at postion 37"<<endl;
+            break;
+        }
+
+        if (current[player] > board)
+        {
+            current[player]-=dice();
+            cout<<"player "<<player+1<<" didn't move as to score on dice more to reach home."<<endl;
+        }
+        else if (current[player]==100)
+        {
+            cout<<"Player "<<player+1<<" wins!!!"<<endl;
+            cout<<"=================================="<<endl;
+            gameOver= true;
+        }
+        
         for (int i = 0; i < maxplayer; i++)
         {
             cout<<"Player "<<i+1<<" is at postion "<<current[i]<<endl;
         }
-        cout<<"Player "<<player+1<<" rolled a "<<dice()<<endl;
-        current[player] += dice();
-        if (current[player] >= board)
-        {
-            cout<<"Player "<<player+1<<" wins!!!"<<endl;
-            gameOver= true;
-        }
-        /* sizeof returns size of datatype e.g if we type sizeof(1) it will return 4 as size of int is 4.
-        so in that way size of float is 4 and char is 1 */
-        for(int i=0; i<sizeof(ladder) / sizeof(ladder[0]); i++){
-			if(current[player] == ladder[i]){
-			cout<<"player "<<player+1<<" climbed a ladder "<<dice()<<endl;
-		    current[player] += dice();
-			}
-		}
 		
-		for(int i=0; i<sizeof(snake) / sizeof(snake[0]); i++){
-			if(current[player] == snake[i]){
-			cout<<"player "<<player+1<<" is bitten by snake "<<dice()<<endl;
-            current[player] -= dice();
-            current[player] -= dice();
-			}
-		}
-
         player = (player+1) % maxplayer;
-        system("pause");
+        // system("pause");
+        cout<<"Press Enter to continue game";
+        cin.ignore();
     }
-    
 }
